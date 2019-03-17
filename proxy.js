9d148b934064674b9fddb024756837d70d411b68
SadeghHayeri <hayerisadegh@gmail.com>
2019-03-17 22:08:36 +0330

    Add main proxy

---
+++ proxy.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const net = require('net');
+const CONFIG = require('./config');
+const RequestHandler = require('./handlers');
+
+const server = net.createServer((socket) => {
+    console.log('client connected');
+
+    socket.on('end', () => {
+        console.log('client disconnected')
+    });
+
+    RequestHandler.handleNewSocket(socket);
+});
+
+server.on('error', (err) => {
+    console.log(err.toString());
+});
+
+server.listen(CONFIG.PROXY.PORT, () => {
+    console.log('server bound');
+});
