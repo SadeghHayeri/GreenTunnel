7dc3e1999d9e964b52a6f8bc91287f1944d8e0d3
SadeghHayeri <hayerisadegh@gmail.com>
2019-03-21 13:17:45 +0330

    Make proxy as a service

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PROXY: {
+        IP: '127.0.0.1',
         PORT: 8000,
         CLIENT_HELLO_MTU: 100,
     }
--- proxy.js
+++ proxy.js
@@ -3,21 +3,40 @@
 const net = require('net');
 const CONFIG = require('./config');
 const RequestHandler = require('./handlers');
+const SystemProxyManager = require('./system-proxy-manager');
 
-const server = net.createServer({ pauseOnConnect: true }, (socket) => {
-    console.log('client connected');
+class Proxy {
+    static async startProxyServer() {
+        Proxy.server = net.createServer({ pauseOnConnect: true }, (socket) => {
+            console.log('client connected');
 
-    socket.on('end', () => {
-        console.log('client disconnected')
-    });
+            socket.on('end', () => {
+                console.log('client disconnected')
+            });
 
-    RequestHandler.handleNewSocket(socket);
-});
+            RequestHandler.handleNewSocket(socket);
+        });
 
-server.on('error', (err) => {
-    console.log(err.toString());
-});
+        Proxy.server.on('error', (err) => {
+            console.log(err.toString());
+        });
 
-server.listen(CONFIG.PROXY.PORT, () => {
-    console.log('server bound');
-});
+        Proxy.server.on('close', () => {
+            console.log('server closed');
+        });
+
+        Proxy.server.listen(CONFIG.PROXY.PORT, CONFIG.PROXY.IP, () => {
+            console.log('server bound');
+        });
+
+        await SystemProxyManager.set_proxy(CONFIG.PROXY.IP, CONFIG.PROXY.PORT);
+    }
+
+    static async stopProxyServer() {
+        Proxy.server.close();
+        await SystemProxyManager.unset_proxy();
+    }
+}
+Proxy.server = null;
+
+module.exports = Proxy;
