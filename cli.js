869b442c26203c1f08c47396afdf75e4bf0b8af2
SadeghHayeri <hayerisadegh@gmail.com>
2019-03-27 12:30:26 +0430

    Add beautiful cli

--- cli.js
+++ cli.js
@@ -2,15 +2,57 @@
 const updateNotifier = require('update-notifier');
 const proxy = require('./proxy');
 const pkg = require('./package.json');
+const chalk = require('chalk');
+const clear = require('clear');
 const debug = require('debug')('cli');
+const ora = require('ora');
 
 updateNotifier({pkg}).notify();
 
-process.on('SIGINT', async () => {
-    debug("Caught interrupt signal");
-    await proxy.stopProxyServer();
-    debug('Successfully Closed!');
-    process.exit();
-});
+const MAIN_COLOR = '84C66F';
 
-proxy.startProxyServer();
+function printBanner() {
+    console.log('                \n' +
+        '                          [0m[48;5;113m    [0m      \n' +
+        '                       [0m[48;5;113m          [0m   \n' +
+        '                      [0m[48;5;113m            [0m  \n' +
+        '                      [0m[48;5;113m     [0m  [0m[48;5;113m     [0m  \n' +
+        '                      [0m[48;5;113m   [0m      [0m[48;5;113m   [0m  \n' +
+        '                      [0m[48;5;113m [0m          [0m[48;5;113m [0m  \n' +
+         '[0m');
+    console.log('                      ' + chalk.hex(MAIN_COLOR).bold('Green') + ' ' + chalk.bold.white('Tunnel'));
+}
+
+function printAlert() {
+    console.log('\n');
+    console.log('    ' + chalk.bgHex(MAIN_COLOR).black(' Note: GreenTunnel does not hide your IP address '))
+}
+
+function showSpiner() {
+    console.log('');
+    const spinner = ora({
+        indent: 27,
+        text: '',
+        color: 'green',
+    }).start();
+}
+
+function main() {
+    clear();
+
+    printBanner();
+    printAlert();
+    showSpiner();
+
+    process.on('SIGINT', async () => {
+        debug("Caught interrupt signal");
+        await proxy.stopProxyServer();
+        debug('Successfully Closed!');
+        clear();
+        process.exit();
+    });
+
+    proxy.startProxyServer();
+}
+
+main();
--- system-proxy-manager.js
+++ system-proxy-manager.js
@@ -90,38 +90,46 @@ class SystemProxyManager {
     }
 
     static async set_proxy(ip, port) {
-        switch (os.platform()) {
-            case 'darwin':
-                await SystemProxyManager._darwin_set_proxy(ip, port);
-                break;
-            case 'linux':
-                await SystemProxyManager._linux_set_proxy(ip, port);
-                break;
-            case 'win32':
-            case 'win64':
-                await SystemProxyManager._win_set_proxy(ip, port);
-                break;
-            case 'unknown os':
-            default:
-                throw 'UNKNOWN OS TYPE ' + os.platform();
+        try {
+            switch (os.platform()) {
+                case 'darwin':
+                    await SystemProxyManager._darwin_set_proxy(ip, port);
+                    break;
+                case 'linux':
+                    await SystemProxyManager._linux_set_proxy(ip, port);
+                    break;
+                case 'win32':
+                case 'win64':
+                    await SystemProxyManager._win_set_proxy(ip, port);
+                    break;
+                case 'unknown os':
+                default:
+                    throw 'UNKNOWN OS TYPE ' + os.platform();
+            }
+        } catch (e) {
+            debug('ERROR SET PROXY ', e);
         }
     }
 
     static async unset_proxy() {
-        switch (os.platform()) {
-            case 'darwin':
-                await SystemProxyManager._darwin_unset_proxy();
-                break;
-            case 'linux':
-                await SystemProxyManager._linux_unset_proxy();
-                break;
-            case 'win32':
-            case 'win64':
-                await SystemProxyManager._win_unset_proxy();
-                break;
-            case 'unknown os':
-            default:
-                throw 'UNKNOWN OS TYPE ' + os.platform();
+        try {
+            switch (os.platform()) {
+                case 'darwin':
+                    await SystemProxyManager._darwin_unset_proxy();
+                    break;
+                case 'linux':
+                    await SystemProxyManager._linux_unset_proxy();
+                    break;
+                case 'win32':
+                case 'win64':
+                    await SystemProxyManager._win_unset_proxy();
+                    break;
+                case 'unknown os':
+                default:
+                    throw 'UNKNOWN OS TYPE ' + os.platform();
+            }
+        } catch (e) {
+            debug('ERROR UNSET PROXY ', e);
         }
     }
 }
