f3bba127328aaa7fbf991e6bbe147a31000abc7f
Lanius-collaris <55432068+Lanius-collaris@users.noreply.github.com>
2023-10-09 07:58:46 +0800

    buffer.js: Implement TLS record fragmentation

--- src/utils/buffer.js
+++ src/utils/buffer.js
@@ -1,4 +1,5 @@
-export function bufferToChunks(buffer, chunkSize) {
+export function bufferToChunks(buffer, chunkSize, recordFragmentation) {
+	if(recordFragmentation) buffer=tlsRecordFragmentation(buffer, chunkSize)
 	const result = [];
 	const len = buffer.length;
 	let i = 0;
@@ -8,4 +9,27 @@ export function bufferToChunks(buffer, chunkSize) {
 	}
 
 	return result;
+}
+/**
+ * @param {Buffer} buffer
+ * @param {number} chunkSize
+ */
+function tlsRecordFragmentation(buffer, chunkSize) {
+	const list = []
+	const header = buffer.subarray(0,3)
+	const fullRecord = buffer.subarray(5)
+	const len = fullRecord.length
+	let i = 0
+
+	while (i < len) {
+		const record = fullRecord.subarray(i, i+chunkSize)
+		const recordLength = record.length
+		const buf = Buffer.alloc(recordLength+5)
+		header.copy(buf)
+		buf.writeUInt16BE(recordLength,3)
+		record.copy(buf,5)
+		list.push(buf)
+		i+=chunkSize
+	}
+	return Buffer.concat(list)
 }
