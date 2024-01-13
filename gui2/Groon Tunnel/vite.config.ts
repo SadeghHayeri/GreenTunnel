5e455df4f0c0187dd4615a4a7dbdc2d24fa26732
Andra Antariksa <andra.antariksa@gmail.com>
2024-01-14 03:09:26 +0700

    feat: add new gui using tauri

---
+++ gui2/Groon Tunnel/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
